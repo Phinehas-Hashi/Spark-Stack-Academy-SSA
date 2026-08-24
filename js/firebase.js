e396e4370a55a024ceb994673a95cb2650657819
Phinehas-Hashi <phinrhaseregae724@gmail.com>
2026-08-24 13:12:50 +0300

    fix: restore last known working Firebase app

--- js/firebase.js
+++ js/firebase.js
@@ -10,11 +10,12 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-
 const firebaseConfig = {
     apiKey: "AIzaSyBlPs-9EU_YYiP4qZ6gFF9ZorJbbktxC4",
     authDomain: "spark-stack-academy.firebaseapp.com",
+    databaseURL: "https://spark-stack-academy-default-rtdb.firebaseio.com",
     projectId: "spark-stack-academy",
     storageBucket: "spark-stack-academy.firebasestorage.app",
     messagingSenderId: "691304828755",
-    appId: "1:691304828755:web:b78b4c47ad007a5be39ba6",
-    measurementId: "G-2CJ4RW1PNZ"
+    appId: "1:691304828755:web:dcb92043eb1a308ce39ba6",
+    measurementId: "G-GYS3P7ZVYJ"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -22,8 +23,6 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
 
-setPersistence(auth, browserLocalPersistence).catch(error => {
-    console.error("Firebase Auth persistence:", error);
-});
+setPersistence(auth, browserLocalPersistence);
 
 export { auth, db, storage };
