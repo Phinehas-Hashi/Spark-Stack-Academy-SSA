2743e65eec35e35531491a371d26ee3e07d57d82
SparkStack Tech <phinehaseregae724@gmail.com>
2026-08-24 12:13:57 +0300

    fix: restore working Firebase web app configuration

--- js/firebase.js
+++ js/firebase.js
@@ -10,12 +10,11 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-
 const firebaseConfig = {
     apiKey: "AIzaSyBlPs-9EU_YYiP4qZ6gFF9ZorJbbktxC4",
     authDomain: "spark-stack-academy.firebaseapp.com",
-    databaseURL: "https://spark-stack-academy-default-rtdb.firebaseio.com",
     projectId: "spark-stack-academy",
     storageBucket: "spark-stack-academy.firebasestorage.app",
     messagingSenderId: "691304828755",
-    appId: "1:691304828755:web:41ef7a43d5e5a51ce39ba6",
-    measurementId: "G-RLXKD9EB4Z"
+    appId: "1:691304828755:web:b78b4c47ad007a5be39ba6",
+    measurementId: "G-2CJ4RW1PNZ"
 };
 
 const app = initializeApp(firebaseConfig);
